bc88e2ba7783a4ce6601aa085c08e438268b0902
AP780 <andrew.pitcairn@students.mq.edu.au>
2026-06-05 22:14:52 +1000

    updated comments

--- src/main/java/com/tiles/server/MetricsController.java
+++ src/main/java/com/tiles/server/MetricsController.java
@@ -9,10 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 import java.io.StringWriter;
 
-@RestController
+@RestController  //sets class as a spring rest controller so it can expose the http endpoints - spring boot configuration
 public class MetricsController {
-
-    @GetMapping(value = "/metrics", produces = MediaType.TEXT_PLAIN_VALUE)
+    
+    @GetMapping(value = "/metrics", produces = MediaType.TEXT_PLAIN_VALUE)    //exposes prometheus metrics as plain text, then scrapes this endpoint to collect the data.
     public String metrics() throws IOException {
         StringWriter writer = new StringWriter();
         TextFormat.write004(writer, CollectorRegistry.defaultRegistry.metricFamilySamples());
