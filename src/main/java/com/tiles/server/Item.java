350f69efbfeef609bcd371698ada41febaee154e
danielshek <daniel.shekhtman@students.mq.edu.au>
2026-06-05 10:56:04 +0000

    Fixed useRequestWithKey() test, by adding equals() override in Item Class, changed some test orders.

--- src/main/java/com/tiles/server/Item.java
+++ src/main/java/com/tiles/server/Item.java
@@ -1,5 +1,7 @@
 package com.tiles.server;
 
+import java.util.Objects;
+
 public class Item {
     
     private String ID;
@@ -8,7 +10,6 @@ public class Item {
     private int spawnX;
     private int spawnY;
 
-
     //constructor
     public Item(String newID, String newDesc, String newType, int newSpawnY, int newSpawnX) {
         this.ID = newID;
@@ -16,6 +17,37 @@ public class Item {
         this.type = newType;
         this.spawnY = newSpawnY;
         this.spawnX = newSpawnX;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        // 1. Check for reference identity (same memory address)
+        if (this == obj) {
+            return true;
+        }
+        
+        // 2. Check for null and ensure exact class type matches
+        if (obj == null || this.getClass() != obj.getClass()) {
+            return false;
+        }
+        
+        // 3. Cast the object to the item target type
+        Item other = (Item) obj;
+        
+        // 4. Compare meaningful primitive and object fields, we will exclude spawn locations as theyre not critical
+        return Objects.equals(this.ID, other.ID) 
+            && Objects.equals(this.description, other.description) 
+            && Objects.equals(this.type, other.type);
+    
+    }
+
+    @Override
+    public int hashCode() {
+        
+        // Always override hashCode alongside equals
+        return Objects.hash(ID, description, type);
+
     }
 
     // Getters
